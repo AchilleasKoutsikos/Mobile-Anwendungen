bcee209a3d518c15c820348e5fb929e83863d769
Achilleas Koutsikos <achilleas93ac@googlemail.com>
2024-05-02 13:23:00 +0200

    Almstrong app update

--- Armstrong_Number_finder_App/armstrong_number_finder_app/lib/main.dart
+++ Armstrong_Number_finder_App/armstrong_number_finder_app/lib/main.dart
@@ -3,18 +3,17 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Armstrong Number App',
       home: ArmstrongNumberScreen(),
     );
@@ -22,16 +21,17 @@ class MainApp extends StatelessWidget {
 }
 
 class ArmstrongNumberScreen extends StatefulWidget {
-  const ArmstrongNumberScreen({super.key});
+  const ArmstrongNumberScreen({Key? key}) : super(key: key);
   @override
   // ignore: library_private_types_in_public_api
   _ArmstrongNumberScreenState createState() => _ArmstrongNumberScreenState();
 }
 
-class _ArmstrongNumberScreenState extends State {
-  final TextEditingController _controller =
-      TextEditingController(); // Create a text controller and use it to retrieve the current value of the TextField.
-  String _result = '';
+class _ArmstrongNumberScreenState extends State<ArmstrongNumberScreen> {
+  final TextEditingController _controller = TextEditingController();
+  String _result = ''; // Variable für das Anzeigeergebnis
+  int _nextArmstrong = 0; // Variable für die nächste Armstrong-Zahl
+  bool _isRunning = false; // Variable, um zu verfolgen, ob der Algorithmus läuft oder nicht
 
   @override
   Widget build(BuildContext context) {
@@ -42,102 +42,101 @@ class _ArmstrongNumberScreenState extends State {
       ),
       body: OrientationBuilder(
         builder: (context, orientation) {
-          return GridView.count(
-            crossAxisCount: orientation == Orientation.portrait ? 1 : 1,
-            padding: const EdgeInsets.all(20.0),
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                      style: TextStyle(fontSize: 20.0),
-                      textAlign: TextAlign.left,
-                      'Starting Number:'),
-                  const SizedBox(height: 40),
+                    'Starting Number:',
+                    style: TextStyle(fontSize: 20.0),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 40), // Abstand
                   TextField(
                     textAlign: TextAlign.right,
                     autofocus: true,
-                    controller: _controller,
+                    controller: _controller, // Steuerung für die Eingabe
                     keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(
                         borderSide: BorderSide(),
                       ),
-                      labelText: '123',
+                      labelText: '123', // Platzhaltertext
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 30,
-                    height: 20,
+                  const SizedBox(height: 20), // Abstand
+                  Container(
+                    alignment: Alignment.centerRight,
                     child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          final int number =
-                              int.tryParse(_controller.text) ?? 0;
-                          if (isArmstrong(number)) {
-                            _result = '$number ist eine Armstrong-Zahl.';
-                          } else {
-                            _result = '$number ist keine Armstrong-Zahl.';
-                          }
-                        });
-                      },
-                      child: const Text('Überprüfen'),
+                      onPressed: _isRunning ? null : _findNextNumber, // Schaltfläche deaktivieren, wenn der Algorithmus läuft
+                      child: const Text('Finde nächste Nummer'),
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 20), // Abstand
                   Text(
-                      style: TextStyle(fontSize: 40.0),
-                      textAlign: TextAlign.center,
-                      _result),
-                  const SizedBox(height: 20),
+                    _result, // Anzeigeergebnis
+                    style: TextStyle(fontSize: 20.0),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20), // Abstand
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: _isRunning ? null : _addNumber, // Schaltfläche deaktivieren, wenn der Algorithmus läuft
+                      child: const Text('Füge die Nummer'),
+                    ),
+                  ),
                 ],
               ),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                width: 10,
-                height: 10,
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      final int number = int.tryParse(_controller.text) ?? 0;
-                      if (isArmstrong(number)) {
-                        _result = '$number ist eine Armstrong-Zahl.';
-                      } else {
-                        _result = '$number ist keine Armstrong-Zahl.';
-                      }
-                    });
-                  },
-                  child: const Text('Überprüfen'),
-                ),
-              ),
-            ],
+            ),
           );
         },
       ),
     );
   }
 
+  void _findNextNumber() async { // Funktion enthält asynchronen Code
+    setState(() { // UI wird aktualisiert
+      _isRunning = true; // Setzen des Flags, dass der Algorithmus läuft
+    });
+
+    final int number = int.tryParse(_controller.text) ?? 0; // Eingegebene Zahl abrufen und in int umwandeln
+    _nextArmstrong = await findNextNum(number + 1); // Warten bis nächste Armstrong-Zahl zu finden
+    _result = 'Die nächste Armstrong-Zahl ist $_nextArmstrong.'; // Ergebnis aktualisieren
+
+    setState(() { // UI wird aktualisiert
+      _isRunning = false; // Setzen des Flags, dass der Algorithmus nicht mehr läuft
+    });
+  }
+
+  Future<int> findNextNum(int number) async { // Funktion gibt zukünftiges Ergebnis von Typ 'int' und enthält asynchronen Code
+    if (isArmstrong(number) == false) { // Überprüfen, ob die Zahl keine Armstrong-Zahl ist
+      await Future.delayed(Duration(milliseconds: 100)); // Eine Verzögerung hinzufügen
+      return findNextNum(number + 1); // Rekursiver Aufruf, um die nächste Zahl zu finden
+    } else {
+      return number; // Die Zahl ist eine Armstrong-Zahl, also zurückgeben
+    }
+  }
+
+  void _addNumber() {
+    setState(() { // UI wird aktualisiert
+      _controller.text = _nextArmstrong.toString(); // Die nächste Armstrong-Zahl zum Eingabefeld hinzufügen
+    });
+  }
+
   bool isArmstrong(int number) {
-    int originalNumber = number;
-    // Initialisierung der Summenvariable
-    int? summe = 0;
+    int originalNumber = number; // Die ursprüngliche Zahl speichern
+    int? summe = 0; // Summe initialisieren
+    var nummerLeange = "$number".length; // Die Länge der Zahl bestimmen
+    String zuString = "$number"; // Zahl in einen String konvertieren
 
-    // Bestimmen der Länge der eingegebenen Zahl
-    var nummerLeange = "$number".length;
-
-    // Konvertieren der eingegebenen Zahl in einen String
-    String zuString = "$number";
-
-    // Durchlaufen jeder Ziffer der eingegebenen Zahl
     for (int i = 0; i < nummerLeange; i++) {
-      // Konvertieren jeder Ziffer von String zu Integer
-      int t = int.parse(zuString[i]);
-      // Hinzufügen der Ziffer, die zur Potenz der Anzahl der Ziffern erhöht ist, zur Summe
-      summe = pow(t, nummerLeange) + summe! as int?;
+      int t = int.parse(zuString[i]); // Jede Ziffer der Zahl erhalten
+      summe = pow(t, nummerLeange) + summe! as int?; // Die Summe der Potenzen der Ziffern berechnen
     }
 
-    return summe == originalNumber;
+    return summe == originalNumber; // Überprüfen, ob die Summe gleich der ursprünglichen Zahl ist
   }
 }
